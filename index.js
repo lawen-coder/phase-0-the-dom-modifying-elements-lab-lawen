b6e00c74fd0fb121f4ffeb5766a870f79863a2f5
lawen <lawensardar16@gmail.com>
2022-08-01 15:03:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+ newHeader.setAttribute('id','victory')
+
+ const message = newHeader.textContent= 'Lawen is the champion'
+
